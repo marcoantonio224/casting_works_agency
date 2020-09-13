8f930ba3b17f015ab95ba2083ed7e48bd532f7d3
Marco Canchola <marcocanchola2020@gmail.com>
2020-09-13 10:16:16 -0700

    Updated server side requests from auth0

--- frontend/src/components/Actors/index.js
+++ frontend/src/components/Actors/index.js
@@ -11,7 +11,7 @@ function Actors() {
   const [actors, setActors] = useState([]);
   const [token, setToken] = useState(false);
   const { user, isAuthenticated , getAccessTokenSilently } = useAuth0()
-  console.log(user)
+
   useEffect(()=>{
 
     const getUserMetadata = async () => {
@@ -35,6 +35,7 @@ function Actors() {
 
   // Send the api token to server for validation
   const res = getData(token, '/actors');
+
   return (
     <section className="main-content-actors">
       <div className="sub-content">
@@ -49,30 +50,23 @@ function Actors() {
                 <p className="text-content" style={{marginTop:'-3%'}}>
                   There are no formal education requirements to become an actor but a bachelor's degree in theater arts, drama, acting and performing, may be helpful in learning technical skills. Experience is of great importance in this career, as experience leads to bigger and higher paying roles. Skills an actor needs include creativity, speaking skills, literacy and reading skills, memorization, physical stamina, persistence, discipline, dedication, and ability to communicate with a wide variety of people.
                 </p>
-                <div className="text-content" style={{marginTop:'-6%'}}>
-                  <h5>5 Easy steps into becoming an actor</h5>
-                  <ul>
-                    <li>Step 1: Take Classes</li>
-                    <li>Step 2: Gain Professional Experience</li>
-                    <li>Step 3: Acquire Additional Skills</li>
-                    <li>Step 4: Find an Agent</li>
-                    <li>Step 5: Advance in the Field</li>
-                  </ul>
-                </div>
               </div>
           </div>
       </div>
       <div>
-          <h3>CastingWorks Actresses & Actors</h3>
+        {(isAuthenticated) ?
           <div>
-            {(actors.length === 0) ? <h4>There are no current actors.</h4> :
-              (!user) ?
-                  'Regular user'
-                  :
-                  'Executive or so'
-
-            }
+            <h3>CastingWorks Actresses & Actors</h3>
+            <div>
+              {(actors.length === 0) ? <h4>There are no current actors.</h4> :
+              <div>Actors</div>}
+            </div>
           </div>
+            :
+            <div>
+              <h3>Not Authenticated</h3>
+            </div>
+        }
       </div>
     </section>
   );
--- frontend/src/components/Movies/index.js
+++ frontend/src/components/Movies/index.js
@@ -8,7 +8,7 @@ import casting from '../../assets/images/casting.jpg';
 function Movies() {
   const [movies, setMovies] = useState([]);
   const [token, setToken] = useState(false);
-  const { user, getAccessTokenSilently } = useAuth0();
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
 
   useEffect(()=>{
     // Async function for movies audience token
@@ -50,28 +50,25 @@ function Movies() {
                   been nominated for an Oscar. We take pride in our producions and
                   continue to make more.
                 </p>
-                <div className="text-content" style={{marginTop:'-6%'}}>
-                  <h5>7 Easy steps into producing a movie</h5>
-                  <ul>
-                    <li>Step 1: Development</li>
-                    <li>Step 2: Pre-Production</li>
-                    <li>Step 3: Production</li>
-                    <li>Step 4: Principal Photography</li>
-                    <li>Step 5: Wrap</li>
-                    <li>Step 6: Post-Production</li>
-                    <li>Step 7: Distribution</li>
-                  </ul>
-                </div>
               </div>
           </div>
       </div>
       <div>
-          <h3>CastingWorks Movies</h3>
+        {(isAuthenticated) ?
           <div>
-            {(movies.length === 0) ? <h4>There are no current movies.</h4> :
-              {/* This is where the movies go */}
-            }
+            <h3>CastingWorks Movies</h3>
+            <div>
+              {
+                (movies.length === 0) ? <h4>There are no current movies.</h4> :
+                <div>Movies</div>
+              }
+            </div>
           </div>
+            :
+          <div>
+            <h3>Not Authenticated</h3>
+          </div>
+        }
       </div>
     </section>
   );
