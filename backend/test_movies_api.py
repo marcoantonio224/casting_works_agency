dcfb5d0fd3c751dc1df4bad613fcea0203e0cc4e
Marco Canchola <marcocanchola2020@gmail.com>
2020-10-03 11:30:57 -0700

    Updated unit testing for all RBCA users

--- backend/test_movies_api.py
+++ backend/test_movies_api.py
@@ -27,12 +27,26 @@ class CastingWorksTestCase(unittest.TestCase):
     self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
     setup_db(self.app, self.database_path)
 
-    # Set Headers (Authorized token)
-    self.headers = {
+    # Set Headers for Producer
+    # Permissions = [[get:movies, post:movies, edit:movies, delete:movies]
+    self.producer_header = {
       'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlluQnRJTEpPUC03ZjBCUEVIelU0ZiJ9.eyJpc3MiOiJodHRwczovL2Nhc3Rpbmd3b3Jrc2ZzbmQudXMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVmNWRhOTM2YThjYWM2MDA2ZmJiYmFhMCIsImF1ZCI6WyJhY3Rpb25zIiwiaHR0cHM6Ly9jYXN0aW5nd29ya3Nmc25kLnVzLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2MDE3MDI1NTgsImV4cCI6MTYwMTc4ODk1OCwiYXpwIjoiSEhYcEtmVWFCOHhyMUVWRjBmZkM2ZWc0dFVhcTYwZE8iLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmFjdG9ycyIsImRlbGV0ZTptb3ZpZXMiLCJlZGl0OmFjdG9ycyIsImVkaXQ6bW92aWVzIiwiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBvc3Q6bW92aWVzIl19.D8VLhT2IcBjXnyLtvBfwN8sKncGMR1ZwinVlXzpe-I0gyu_lBNMmyGKxeYW6KgW5TmtepbLEyvB5TK1eMyXjUJcYqg5am9KJPPRB7rorvO-fRUKlCWz_kxJUSiUNsmthsdkLaHpcpOGJu108Xi3HXE_IMz0KzGz0ZqOfOf5NciGeWhDgbyVMGhp9lN1vWZgdM3NePZE3yqEAH7TMJM6HzLMUSmQnx9gcqktlaycGabpw9JCeTi_Wym-3OF8bepOrt5UXgDv-3UdJ_B4o7-vIJeSXUHZkf4Y64v1m7npY16b0zPP4iZSyUctsrFPpcbtz2SJVN51WSt0sxl1kdmk2AA',
       'Content-Type': 'application/json'
     }
 
+    # Set Headers for Director
+    # Permissions = [get:movies, edit:movies]
+    self.director_header = {
+      'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlluQnRJTEpPUC03ZjBCUEVIelU0ZiJ9.eyJpc3MiOiJodHRwczovL2Nhc3Rpbmd3b3Jrc2ZzbmQudXMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVmNWRhNTFhMWQ4MGIxMDA3OGU1ZTZhYiIsImF1ZCI6WyJhY3Rpb25zIiwiaHR0cHM6Ly9jYXN0aW5nd29ya3Nmc25kLnVzLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2MDE3NDgzMDIsImV4cCI6MTYwMTgzNDcwMiwiYXpwIjoiSEhYcEtmVWFCOHhyMUVWRjBmZkM2ZWc0dFVhcTYwZE8iLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmFjdG9ycyIsImVkaXQ6YWN0b3JzIiwiZWRpdDptb3ZpZXMiLCJnZXQ6YWN0b3JzIiwiZ2V0Om1vdmllcyIsInBvc3Q6YWN0b3JzIl19.Ayy_G9k7cfg4xinPmrcOw5l5IPFSpfG1l5SlW0JCK4G-2wR-0Sf1K-zf06ijiYyV_48RuoBXXdlve_3H-WMb9ALlSn5AD1VUaIa6cQs8qAlPSBNqTzi0X1lJDLm4rpFI8mkrts1v-FuqMIxLr4Y-2NwNlQItgQ714LN6Zs6qVgQgJ9gOQkbrBYH09xwHdcD0em_WZYFxHi5iIupA187JFZIwtwrUSU-x080BCGezFqA_PHwj1kVSRTDfoK5CaQOyqySNLEMVuikDEbkd5jDF8OrUg4imQIYWmCeomXKjjxVnB-qIj8hBBjCg5_OtCFEpJQTT3a9TtKn0n7UsoJ-3DQ',
+      'Content-Type': 'application/json'
+    }
+
+    # Set Headers for Assistant
+    # Permission = [get:movies]
+    self.assistant_header = {
+      'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlluQnRJTEpPUC03ZjBCUEVIelU0ZiJ9.eyJpc3MiOiJodHRwczovL2Nhc3Rpbmd3b3Jrc2ZzbmQudXMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVmNTcxNzVlMjA3NmE3MDA2NzhmY2VmMSIsImF1ZCI6WyJhY3Rpb25zIiwiaHR0cHM6Ly9jYXN0aW5nd29ya3Nmc25kLnVzLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2MDE3NDc1MDMsImV4cCI6MTYwMTgzMzkwMywiYXpwIjoiSEhYcEtmVWFCOHhyMUVWRjBmZkM2ZWc0dFVhcTYwZE8iLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwicGVybWlzc2lvbnMiOlsiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiXX0.AYUBIq_SOd-vKEwHZY_l1Pe0rChVwGbCPDg6g2LfinaZHSHr87r0RPqOx7bE0qzUkipmqvNLfeTLXbQ3SdcclKxmlmD6inat7m5uMZPB_l2MPxbf5lgmCXElNlmBfW7KC_irlAFZz9dvxfb3jXCav_5aXuv5svCzPbspLwj3qBcvxNbNMQB9ZPNly1FH9tUnesUD6N36LsmXVKG7GY-IEc3WivLTcjARhq3hBRVSlXDxq3pMjH2Pdo6Yb3RFfWgT176hwFcwNUP4JBfIbJc5cWGXmJ9ViGNu1AZ2MG6nvEwdaPd2xXDunfygzp6JKwDhFTN1s4kPDzlCfU6Qll4NOQ',
+      'Content-Type': 'application/json'
+    }
     # Set unauthorized token
     self.headers_unauth = {
       'Authorization': 'Bearer token',
@@ -65,9 +79,27 @@ class CastingWorksTestCase(unittest.TestCase):
     pass
 
   #============== GET /movies =================
-  # Success Request
-  def test_get_movies_success(self):
-    response = self.client().get("/movies", headers=self.headers)
+  # Producer Success Request
+  def test_producer_get_movies_success(self):
+    response = self.client().get("/movies", headers=self.producer_header)
+    data = json.loads(response.data)
+    # Assertions
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertTrue(data['movies'])
+
+  # Director Success Request
+  def test_director_get_movies_success(self):
+    response = self.client().get("/movies", headers=self.director_header)
+    data = json.loads(response.data)
+    # Assertions
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(data['success'], True)
+    self.assertTrue(data['movies'])
+
+  # Assistant Success Request
+  def test_assistant_get_movies_success(self):
+    response = self.client().get("/movies", headers=self.assistant_header)
     data = json.loads(response.data)
     # Assertions
     self.assertEqual(response.status_code, 200)
@@ -85,7 +117,7 @@ class CastingWorksTestCase(unittest.TestCase):
 
   # Failed Request
   def test_get_movies_failure(self):
-    response = self.client().get( "/moviesFail", headers=self.headers)
+    response = self.client().get( "/moviesFail", headers=self.producer_header)
     data = json.loads(response.data)
     # Assertions
     self.assertEqual(response.status_code, 404)
@@ -93,14 +125,32 @@ class CastingWorksTestCase(unittest.TestCase):
     self.assertEqual(data['message'], "Resource not found")
 
   #============== POST /movies =================
-  # Success Request
-  def test_create_movie_success(self):
-    response = self.client().post( '/movies', headers=self.headers, data=json.dumps(self.new_movie))
+  # Producer Success Request
+  def test_producer_create_movie_success(self):
+    response = self.client().post( '/movies', headers=self.producer_header, data=json.dumps(self.new_movie))
     data = json.loads(response.data)
     # Assertions
     self.assertEqual(response.status_code, 200)
     self.assertTrue(data['new_movie'])
     self.assertEqual(data['success'], True)
+
+  # Director Success Request
+  def test_director_create_movie_success(self):
+    response = self.client().post( '/movies', headers=self.director_header, data=json.dumps(self.new_movie))
+    data = json.loads(response.data)
+    # Assertions
+    self.assertEqual(response.status_code, 401)
+    self.assertEqual(data['success'], False)
+    self.assertEqual(data['message'], 'Unauthorized')
+
+  # Assistant Success Request
+  def test_assistant_create_movie_success(self):
+    response = self.client().post( '/movies', headers=self.assistant_header, data=json.dumps(self.new_movie))
+    data = json.loads(response.data)
+    # Assertions
+    self.assertEqual(response.status_code, 401)
+    self.assertEqual(data['success'], False)
+    self.assertEqual(data['message'], 'Unauthorized')
 
   # Unauthorized Request
   def test_create_movie_unauthorized(self):
@@ -113,7 +163,7 @@ class CastingWorksTestCase(unittest.TestCase):
 
   # Failed Request
   def test_create_movie_failure(self):
-    response = self.client().post( "/movies", headers=self.headers, data=json.dumps(self.new_movie_fail))
+    response = self.client().post( "/movies", headers=self.producer_header, data=json.dumps(self.new_movie_fail))
     data = json.loads(response.data)
     # Assertions
     self.assertEqual(response.status_code, 422)
@@ -121,15 +171,36 @@ class CastingWorksTestCase(unittest.TestCase):
     self.assertEqual(data['message'], "Unprocessable")
 
   #============== PATCH /movies =================
-  # Success Request
-  def test_edit_movie_success(self):
+  # Producer Success Request
+  def test_producer_edit_movie_success(self):
     updated_movie = self.new_movie
-    response = self.client().patch( '/movies/{}'.format(self.movie_id), headers=self.headers, data=json.dumps(updated_movie))
+    response = self.client().patch( '/movies/{}'.format(self.movie_id), headers=self.producer_header, data=json.dumps(updated_movie))
     data = json.loads(response.data)
     # Assertions
     self.assertEqual(response.status_code, 200)
     self.assertTrue(data['updated_movie'])
     self.assertEqual(data['success'], True)
+
+  # Director Success Request
+  def test_director_edit_movie_success(self):
+    updated_movie = self.new_movie
+    response = self.client().patch( '/movies/{}'.format(self.movie_id), headers=self.director_header, data=json.dumps(updated_movie))
+    data = json.loads(response.data)
+    # Assertions
+    self.assertEqual(response.status_code, 200)
+    self.assertTrue(data['updated_movie'])
+    self.assertEqual(data['success'], True)
+
+
+  # Assistant Success Request
+  def test_assistant_edit_movie_success(self):
+    updated_movie = self.new_movie
+    response = self.client().patch( '/movies/{}'.format(self.movie_id), headers=self.assistant_header, data=json.dumps(updated_movie))
+    data = json.loads(response.data)
+    # Assertions
+    self.assertEqual(response.status_code, 401)
+    self.assertEqual(data['success'], False)
+    self.assertEqual(data['message'], 'Unauthorized')
 
   # Unauthorized Request
   def test_edit_movie_unauthorized(self):
@@ -142,7 +213,7 @@ class CastingWorksTestCase(unittest.TestCase):
 
   # Failed Request
   def test_cedit_movie_failure(self):
-    response = self.client().patch( '/movies/{}'.format(self.movie_id), headers=self.headers, data=json.dumps(self.new_movie_fail))
+    response = self.client().patch( '/movies/{}'.format(self.movie_id), headers=self.producer_header, data=json.dumps(self.new_movie_fail))
     data = json.loads(response.data)
     # Assertions
     self.assertEqual(response.status_code, 422)
@@ -150,14 +221,35 @@ class CastingWorksTestCase(unittest.TestCase):
     self.assertEqual(data['message'], "Unprocessable")
 
   #============== DELETE /movies ===============
-  def test_delete_movie_success(self):
+  # Producer Success Request
+  def test_producer_delete_movie_success(self):
     updated_actor = self.new_movie
-    response = self.client().delete( '/movies/{}'.format(self.delete_movie_id), headers=self.headers)
+    response = self.client().delete( '/movies/{}'.format(self.delete_movie_id), headers=self.producer_header)
     data = json.loads(response.data)
     # Assertions
     self.assertEqual(response.status_code, 200)
     self.assertEqual(data['success'], True)
     self.assertEqual(data['delete'], self.delete_movie_id)
+
+  # Director Success Request
+  def test_director_delete_movie_success(self):
+    updated_actor = self.new_movie
+    response = self.client().delete( '/movies/{}'.format(self.delete_movie_id), headers=self.director_header)
+    data = json.loads(response.data)
+    # Assertions
+    self.assertEqual(response.status_code, 401)
+    self.assertEqual(data['success'], False)
+    self.assertEqual(data['message'], 'Unauthorized')
+
+  # Assistant Success Request
+  def test_assistant_delete_movie_success(self):
+    updated_actor = self.new_movie
+    response = self.client().delete( '/movies/{}'.format(self.delete_movie_id), headers=self.assistant_header)
+    data = json.loads(response.data)
+    # Assertions
+    self.assertEqual(response.status_code, 401)
+    self.assertEqual(data['success'], False)
+    self.assertEqual(data['message'], 'Unauthorized')
 
   # Unauthorized Request
   def test_delete_movie_unauthorized(self):
@@ -170,7 +262,7 @@ class CastingWorksTestCase(unittest.TestCase):
 
   # Failed Request
   def test_delete_movie_failure(self):
-    response = self.client().delete( '/movies/dsds{}'.format(self.delete_movie_id), headers=self.headers)
+    response = self.client().delete( '/movies/dsds{}'.format(self.delete_movie_id), headers=self.producer_header)
     data = json.loads(response.data)
     # Assertions
     self.assertEqual(response.status_code, 404)
